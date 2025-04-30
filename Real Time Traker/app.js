bf1a7a80e4968beccf77e61ca554d1eb128fb3c6
Your Name <sheikhabdullah456677@gmail.com>
2025-05-01 02:24:17 +0500

    first commit

---
+++ Real Time Traker/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const http = require("http")
+
+const socketio = require("socket.io")
+
+const server = http.createServer (app)
+const io = socketio(server)
+
+
+app.set("view engine", "ejs")
+app.set(express.static((__dirname , "public")))
+
+app.get("/", function (req, res ) {
+    res.send ("hey ")
+})
+server.listen(3000)
